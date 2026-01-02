2984da4a8da99ac81c1d5c08d9f0de13a7addd8e
steven <steven.carlotpro@gmail.com>
2026-01-02 23:51:45 +0100

    J'ai commencé a faire l'interface graphique avec streamlit

---
+++ src/visualisations.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Trouvez le métier fait pour vous.')
+with st.form(key="formulaire", clear_on_submit=True, enter_to_submit=True, border=True, width="stretch", height="content"):
+    Categorie = st.multiselect("Domaine qui vous interesse",["Juridique","Business","Stratégie","Marketing","Finance","Comptabilité",
+                                           "Communication","Design","Digital et Réseaux Sociaux","Data Analysis",
+                                           "Machine Learning","Intelligence Artificielle","Développement","Ingénierie Technique"])
+
+    Experience_passe = st.text_area(
+        "Décrivez un projet technique dont vous êtes fier. Quelles technologies avez-vous utilisées et quels problèmes avez-vous résolus ?",
+    )
+
+    st.write(f"You wrote {len(Experience_passe)} characters.")
+
+    Mission_quotidienne = st.text_area(
+        "Si vous deviez décrire votre journée de travail idéale en termes de tâches techniques, que diriez-vous ?",
+    )
+
+    st.write(f"You wrote {len(Mission_quotidienne)} characters.")
+
+    Negociation = st.text_area(
+        "Aimez-vous débattre, argumenter et convaincre des interlocuteurs dans un cadre professionnel ?",
+    )
+
+    st.write(f"You wrote {len(Mission_quotidienne)} characters.")
+    Rigueur = st.text_area(
+        "Préférez-vous un travail exigeant une précision millimétrée (chiffres, clauses) ou un travail laissant place à l'improvisation créative ?",
+    )
+
+    st.write(f"You wrote {len(Mission_quotidienne)} characters.")
+
+    st.form_submit_button(label="Submit")
+
+st.write(Categorie)
+st.write(Rigueur)
